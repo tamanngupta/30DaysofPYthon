70f4b3e69c9d30df7ff8d54e1743c696f3edd31e
tamanna gupta <tamannagupta0097@gmail.com>
2026-06-09 18:16:57 +0530

    euclidean distance

---
+++ Day 1 ex/lvl3-2.py
@@ -0,0 +1,10 @@
+import math
+
+#defining coordinates of the points
+A = (2, 3)
+B = (10, 8)
+
+#calculating the distance between the points
+distance = math.dist(A, B)
+
+print(f"The distance between points A{A} and B{B} is: {distance}")
