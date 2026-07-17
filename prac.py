36ef9032ed463556d5adbd15c213844e2a8c991e
tamanna gupta <tamannagupta0097@gmail.com>
2026-07-18 00:48:19 +0530

    2 ques

--- prac.py
+++ prac.py
@@ -1,3 +1,4 @@
+
 lst = []
 
 for i in range(2000, 3200):
@@ -5,3 +6,18 @@ for i in range(2000, 3200):
         lst.append(i)
 
 print(lst)
+
+
+
+
+
+def accepting_lists(strr):
+    m = strr.split(',')
+
+    lst = []
+    for i in m: 
+        lst.append(i)
+
+    return lst, tuple(lst)
+
+print(accepting_lists('43,5432,5423,32,21,32'))
