c2006b3489a77904a9e41b8f919933360561f2fa
tamanna gupta <tamannagupta0097@gmail.com>
2026-07-20 08:27:20 +0530

    a bit more practice

--- valid.py
+++ valid.py
@@ -4,10 +4,10 @@ email = input('whats your email?: ').strip()
 #re.search(what to find, where to find, what to flag)
 #  . is any charecter except a new line, * 0 or more repetition, + 1 or more repetitions, ? 0 or 1 repetition, this is for how many times you want a specific charecter to repeat eg/ {m} means m repetitions of say digits 
 
-if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]]+\.edu$", email): 
+if re.search(r"^\w+@(\w+\.)?\w+\.(edu|org|com)$", email, re.IGNORECASE):
     print('valid')
 else: 
     print('invalid ')
 
-
-#use hypen for a through z no adding spaces or commmas 
+#re.IGNORECASE  - allows u to ignore the case or be case insensitive without actually changing the variable value 
+#. right now cannot be accepetd after @ and before .  but technically there could be other email addressess
