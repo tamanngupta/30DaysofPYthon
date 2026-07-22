40f3710e50416785696194163818ee9a893a56cf
tamanna gupta <tamannagupta0097@gmail.com>
2026-07-22 23:43:37 +0530

    push to prod

--- valid.py
+++ valid.py
@@ -4,10 +4,12 @@ email = input('whats your email?: ').strip()
 #re.search(what to find, where to find, what to flag)
 #  . is any charecter except a new line, * 0 or more repetition, + 1 or more repetitions, ? 0 or 1 repetition, this is for how many times you want a specific charecter to repeat eg/ {m} means m repetitions of say digits 
 
-if re.search(r"^\w+@(\w+\.)?\w+\.(edu|org|com)$", email, re.IGNORECASE):
+if re.search(r"^(\w|\.)a+@(\w+\.)?\w+\.(edu|org|com)$", email, re.IGNORECASE):
     print('valid')
 else: 
     print('invalid ')
 
 #re.IGNORECASE  - allows u to ignore the case or be case insensitive without actually changing the variable value 
 #. right now cannot be accepetd after @ and before .  but technically there could be other email addressess
+
+print('shit I dont wanna miss today')
