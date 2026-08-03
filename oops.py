8630301c9ecbde3ca1bc3a54ee28345c84b0e0d9
Tamanna Gupta <tamannagupta0097@gmail.com>
2026-08-03 05:33:06 +0530

    Refactor Student class with property methods
    Refactor Student class to use properties for name and house. Add validation in setters and update string representation.

--- oops.py
+++ oops.py
@@ -1,44 +1,40 @@
 class Student:
-    #when a function is inside a class its known as method. init and str are special methods that python has built in 
     def __init__(self, name, house):
         self.name = name
-        self.house = house 
+        self.house = house
+      #so this line and main will go through our setters 
+
+    def __str__(self):
+        return f"{self.name} from {self._house}"
+
+    @property
+    def house(self):
+        return self._house
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
         if not name:
             raise ValueError
-        if house not in ['Gryffindor', 'slytherin', 'Hufflepuff', 'RavenClaw']:
-            raise ValueError('Invalid house name')
-    
-    def __str__(self):
-        return f"{self.name} from {self.house}."
-
-
-#getter
-    def house(self):
-        return self.house
-#setter
+        self._name = name
+    @house.setter
     def house(self, house):
-        if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'slytherin']
-        self.house = house 
-#a getter is a function for the class that gets some attribute. a setter sets some value. 
-
-#whenever you create a function in a class it always must take one attribute which is self to give you access to the current object in question
-   
-#we can circumvent conditions that are built in the classes by harcoding them later on
-#therefore even thought we have more control over the code we can still modifyl things 
-#a property is just an attribute that has more defence mechanisms in plac. It is a function in python. IT IS A DECORATOR.
+        if house.lower() not in ['slytherin', 'gryffindor', 'hufflepuff', 'ravenclaw']:
+            raise ValueError('Invalid house name')
+        self._house = house
 
 def main():
     student = get_student()
     print(student)
-
+    
 def get_student():
     name = input('name: ')
-    house = input('house: ')
+    house = input('house')
     student = Student(name, house)
-    return student
-
-
-
+    return student 
 
 if __name__ == '__main__':
     main()
