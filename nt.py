19c1fbefe5676037ae25e4951e6c02ab0ed2bc14
Tamanna Gupta <tamannagupta0097@gmail.com>
2026-08-04 20:48:02 +0530

    Create nt.py with numpy and matplotlib setup
    Initialize nt.py with numpy and matplotlib imports and basic comments.

---
+++ nt.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pylab as plt 
+from numpy import random
+
+#panda builds on numpy library 
+
+
+lst = [1, 2, 3, 4]
+np_arr_1 = np.array(lst, )
+
+
+#multidimensional array library = so list is one dimensional 
+
+
+#lists are super slow due to lack of fixed types, arrays only store one type of value and use vectorization 
